6838dcb65352da0dd6801a63115217cf58f7dc07
Michelle Sharer <michellesharer@gmail.com>
2024-05-08 11:42:47 -0600

    Line bounces back and forth. Add fade shape.

--- prototype/Line.h
+++ prototype/Line.h
@@ -8,8 +8,8 @@ private:
 
   void _updatePosition() {
     _position += _speed;
-    if (_position >= _path.length) {
-      _position = 0;
+    if (_position >= _path.length || (_position < _length && _speed < 0)) {
+      _speed = -_speed;
     }
   }
 
@@ -18,24 +18,20 @@ public:
 
   static constexpr Range SPEED = {1, 10, 3};
   static constexpr Range LENGTH = {
-      DEPTH_SEGMENT_LENGTH, DEPTH_SEGMENT_LENGTH * 3, DEPTH_SEGMENT_LENGTH};
+      DEPTH_SEGMENT_LENGTH, DEPTH_SEGMENT_LENGTH * 3, DEPTH_SEGMENT_LENGTH * 2};
 
-  void setPosition(float position) {
-    _position = position;
-  }
+  void setPosition(float position) { _position = position; }
 
-  void setSpeed(float speed) {
-    _speed = speed;
-  }
+  void setSpeed(float speed) { _speed = speed; }
 
-  void setPath(Path &path) {
-    _path = path;
-  }
+  void setPath(Path &path) { _path = path; }
 
   void show() {
     // show this Line at current position and add tail of length _length
     for (int i = _position; i > _position - _length && i > 0; i--) {
-      _path.leds[i] = palette.getColor(_path.offset + i);
+      int brightness =
+          addFadeShape(map(i, _position, _position - _length, 0, 255));
+      _path.leds[i] = palette.getColor(_path.offset + i).nscale8(brightness);
     }
 
     _updatePosition();
