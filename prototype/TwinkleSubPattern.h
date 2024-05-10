3c1cd8ea2c0e84e0eab08050492bd726fedecda1
Michelle Sharer <michellesharer@gmail.com>
2024-05-10 10:22:02 -0600

    Twinkle cleanup

--- prototype/TwinkleSubPattern.h
+++ prototype/TwinkleSubPattern.h
@@ -4,10 +4,6 @@ private:
   uint8_t _activeSubPattern = 0;
   uint8_t _percentBrightness = 0; // percent brightness of the whole pattern
 
-  void _showTwinkle() { _twinkle.show(); }
-
-  void _showRandomFadingSegments() { _twinkle.show(); }
-
 public:
   static const uint8_t TWINKLE = 0;
   static const uint8_t RANDOM_FADING_SEGMENTS = 1;
@@ -18,8 +14,6 @@ public:
 
   void setup() {
     switch (_activeSubPattern) {
-    case TWINKLE:
-      break;
     case RANDOM_FADING_SEGMENTS:
       _twinkle.setDensityMultiplier(0.5);
       _twinkle.setSpeedMultiplier(2);
@@ -40,10 +34,8 @@ public:
   virtual void show() {
     switch (_activeSubPattern) {
     case TWINKLE:
-      _showTwinkle();
-      break;
     case RANDOM_FADING_SEGMENTS:
-      _showRandomFadingSegments();
+      _twinkle.show();
       break;
     default:
       break;
