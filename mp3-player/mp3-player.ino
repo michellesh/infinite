87d99a84627bce846cee8b5354634e82d0818cfc
Michelle Sharer <michellesharer@gmail.com>
2024-05-13 13:02:15 -0600

    Read audio signal from MP3 player and compute FFT, display to LED matrix

--- mp3-player/mp3-player.ino
+++ mp3-player/mp3-player.ino
@@ -14,6 +14,8 @@
  *  RX2 (GPIO 16) | TX
  *  TX2 (GPIO 17) | RX
  */
+#include <FastLED_NeoMatrix.h>
+#include <arduinoFFT.h>
 
 #define CMD_PLAY_NEXT 0x01
 #define CMD_PLAY_PREV 0x02
@@ -29,24 +31,170 @@
 #define SINGLE_CYCLE_ON 0x00
 #define SINGLE_CYCLE_OFF 0x01
 
+#define AUDIO_IN_PIN 35
+#define SAMPLES 1024
+#define SAMPLING_FREQ 40000
+
+
+#define SAMPLES         1024          // Must be a power of 2
+#define SAMPLING_FREQ   10240         // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define AMPLITUDE       1000          // Depending on your audio source level, you may need to alter this value. Can be used as a 'sensitivity' control.
+#define AUDIO_IN_PIN    35            // Signal in on this pin
+#define NUM_BANDS       16            // To change this, you will need to change the bunch of if statements describing the mapping from bins to bands
+#define NOISE           500           // Used as a crude noise filter, values below this are ignored
+#define TOP            16                // Don't allow the bars to go offscreen
+
+#define MAX_MILLIAMPS   2000          // Careful with the amount of power here if running off USB port
+const int BRIGHTNESS_SETTINGS[3] = {5, 70, 200};  // 3 Integer array for 3 brightness settings (based on pressing+holding BTN_PIN)
+#define LED_VOLTS       5             // Usually 5 or 12
+#define CHIPSET         WS2812B       // LED strip type
+#define COLOR_ORDER     GRB           // If colours look wrong, play with this
+#define LED_PIN         13             // LED strip data
+const uint8_t kMatrixWidth = 16;                          // Matrix width
+const uint8_t kMatrixHeight = 16;                         // Matrix height
+#define NUM_LEDS       (kMatrixWidth * kMatrixHeight)     // Total number of LEDs
+#define BAR_WIDTH      (kMatrixWidth  / (NUM_BANDS - 1))  // If width >= 8 light 1 LED width per bar, >= 16 light 2 LEDs width bar etc
+
+
+// Sampling and FFT stuff
+unsigned int sampling_period_us;
+byte peak[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};              // The length of these arrays must be >= NUM_BANDS
+int oldBarHeights[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int bandValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+unsigned long newTime;
+//ArduinoFFT FFT = ArduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQ);
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, SAMPLES, SAMPLING_FREQ);
+
+
+CRGB leds[NUM_LEDS];
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds, kMatrixWidth, kMatrixHeight,
+  NEO_MATRIX_TOP        + NEO_MATRIX_LEFT +
+  NEO_MATRIX_ROWS       + NEO_MATRIX_ZIGZAG +
+  NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS);
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
   delay(500); // wait chip initialization is complete
 
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+  FastLED.setMaxPowerInVoltsAndMilliamps(LED_VOLTS, MAX_MILLIAMPS);
+  FastLED.setBrightness(BRIGHTNESS_SETTINGS[1]);
+  FastLED.clear();
+
   mp3_command(CMD_SEL_DEV, DEV_TF); // select the TF card
   delay(200);                       // wait for 200ms
 
   mp3_command(CMD_SINGLE_CYCLE, SINGLE_CYCLE_ON); // Plays the song on repeat
-  mp3_command(CMD_PLAY_W_INDEX, 3);               // Play track number 3
+  mp3_command(CMD_PLAY, 0x0000); // Play first track
+  //mp3_command(CMD_PLAY_W_INDEX, 3);               // Play track number 3
 
   // mp3_command(CMD_PAUSE, 0x0000);      // Pause mp3
   // mp3_command(CMD_PLAY_NEXT, 0x0000);  // Play next mp3
   // mp3_command(CMD_PLAY_PREV, 0x0000);  // Play previous mp3
   // mp3_command(CMD_SET_VOLUME, 30);     // Change volume to 30
+
+  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
 }
 
-void loop() {}
+void loop() {
+
+  FastLED.clear();
+
+  // Reset bandValues[]
+  for (int i = 0; i<NUM_BANDS; i++){
+    bandValues[i] = 0;
+  }
+
+  // Sample the audio pin
+  for (int i = 0; i < SAMPLES; i++) {
+    newTime = micros();
+    vReal[i] = analogRead(AUDIO_IN_PIN); // A conversion takes about 9.7uS on an ESP32
+    vImag[i] = 0;
+    while ((micros() - newTime) < sampling_period_us) { /* chill */ }
+  }
+
+  // Compute FFT
+  FFT.dcRemoval();
+  FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.compute(FFT_FORWARD);
+  FFT.complexToMagnitude();
+
+  // Analyse FFT results
+  for (int i = 2; i < (SAMPLES/2); i++){       // Don't use sample 0 and only first SAMPLES/2 are usable. Each array element represents a frequency bin and its value the amplitude.
+    if (vReal[i] > NOISE) {                    // Add a crude noise filter
+
+    /*8 bands, 12kHz top band
+      if (i<=3 )           bandValues[0]  += (int)vReal[i];
+      if (i>3   && i<=6  ) bandValues[1]  += (int)vReal[i];
+      if (i>6   && i<=13 ) bandValues[2]  += (int)vReal[i];
+      if (i>13  && i<=27 ) bandValues[3]  += (int)vReal[i];
+      if (i>27  && i<=55 ) bandValues[4]  += (int)vReal[i];
+      if (i>55  && i<=112) bandValues[5]  += (int)vReal[i];
+      if (i>112 && i<=229) bandValues[6]  += (int)vReal[i];
+      if (i>229          ) bandValues[7]  += (int)vReal[i];*/
+
+    //16 bands, 12kHz top band
+      if (i<=2 )           bandValues[0]  += (int)vReal[i];
+      if (i>2   && i<=3  ) bandValues[1]  += (int)vReal[i];
+      if (i>3   && i<=5  ) bandValues[2]  += (int)vReal[i];
+      if (i>5   && i<=7  ) bandValues[3]  += (int)vReal[i];
+      if (i>7   && i<=9  ) bandValues[4]  += (int)vReal[i];
+      if (i>9   && i<=13 ) bandValues[5]  += (int)vReal[i];
+      if (i>13  && i<=18 ) bandValues[6]  += (int)vReal[i];
+      if (i>18  && i<=25 ) bandValues[7]  += (int)vReal[i];
+      if (i>25  && i<=36 ) bandValues[8]  += (int)vReal[i];
+      if (i>36  && i<=50 ) bandValues[9]  += (int)vReal[i];
+      if (i>50  && i<=69 ) bandValues[10] += (int)vReal[i];
+      if (i>69  && i<=97 ) bandValues[11] += (int)vReal[i];
+      if (i>97  && i<=135) bandValues[12] += (int)vReal[i];
+      if (i>135 && i<=189) bandValues[13] += (int)vReal[i];
+      if (i>189 && i<=264) bandValues[14] += (int)vReal[i];
+      if (i>264          ) bandValues[15] += (int)vReal[i];
+    }
+  }
+
+  int sum = 0;
+  // Process the FFT data into bar heights
+  for (byte band = 0; band < NUM_BANDS; band++) {
+
+    // Scale the bars for the display
+    int barHeight = bandValues[band] / AMPLITUDE;
+    if (barHeight > TOP) barHeight = TOP;
+
+    // Small amount of averaging between frames
+    barHeight = ((oldBarHeights[band] * 1) + barHeight) / 2;
+
+    // Move peak up
+    if (barHeight > peak[band]) {
+      peak[band] = min(TOP, barHeight);
+    }
+
+    sum += barHeight;
+    // Draw bars
+    rainbowBars(band, barHeight);
+
+    // Draw peaks
+
+    // Save oldBarHeights for averaging later
+    oldBarHeights[band] = barHeight;
+  }
+
+  Serial.println(sum);
+
+  FastLED.show();
+}
+
+void rainbowBars(int band, int barHeight) {
+  int xStart = BAR_WIDTH * band;
+  for (int x = xStart; x < xStart + BAR_WIDTH; x++) {
+    for (int y = TOP; y >= TOP - barHeight; y--) {
+      matrix->drawPixel(x, y, CHSV((x / BAR_WIDTH) * (255 / NUM_BANDS), 255, 255));
+    }
+  }
+}
 
 void mp3_command(int8_t command, int16_t dat) {
   int8_t frame[8] = {0};
