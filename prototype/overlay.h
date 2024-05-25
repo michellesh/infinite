4a4f19816cf32b6c404bee50a7e637763c160b03
Michelle Sharer <michellesharer@gmail.com>
2024-05-24 18:13:32 -0600

    Wave overlays to BPM

--- prototype/overlay.h
+++ prototype/overlay.h
@@ -4,7 +4,7 @@ void waveOverlay() {
   int offset = map(overlayDensity, 1, 10, MAX_DEPTH, 0) + width / 2;
   int startPosition = reverse ? -offset : MAX_DEPTH + offset;
   int endPosition = reverse ? MAX_DEPTH + offset : -offset;
-  int position = sawtooth(startPosition, endPosition, waveLength);
+  int position = mapBeat(startPosition, endPosition);
   for (int i = 0; i < NUM_LEDS; i++) {
     int depth = ledDepth(i);
     int distFromPosition = abs(depth - position);
