c951a0e23127270f58351bd60ef4a25f25df4359
Michelle Sharer <michellesharer@gmail.com>
2024-05-29 15:33:28 -0600

    Fix sequence

--- libraries/InfiniteShared/actions.h
+++ libraries/InfiniteShared/actions.h
@@ -6,15 +6,16 @@ Action actions[] = {
     time(5000).bpm(224).pattern(PATTERN_LASERS_DOUBLES),
     time(10000).pattern(PATTERN_LASERS),
     time(15000).pattern(PATTERN_ROTATING_HEXAGONS).speed(9.5).width(10),
-    time(20000).speed(1),
-    time(25000).width(1).speed(6).density(1),
+    time(20000).bpm(56),
+    time(25000).width(1).density(1),
     time(30000)
         .pattern(PATTERN_TWINKLE)
         .width(5)
         .density(4)
         .speed(7)
         .overlay(OVERLAY_BRIGHTNESS_SINWAVE)
-        .bpm(112)};
+        .bpm(112)
+};
 
 Fade fades[] = {
     fadeOut(5000, 10000),
